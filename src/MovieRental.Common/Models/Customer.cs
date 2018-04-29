f80b2d2698899ae1b135442a5ff0cd872536c1fe
Rafał Pieńkowski <rafal.pienkowski@wp.pl>
2018-04-29 18:54:52 +0200

    Intorude of GetTotalFrequentRenterPoints (with LINQ)

--- src/MovieRental.Common/Models/Customer.cs
+++ src/MovieRental.Common/Models/Customer.cs
@@ -21,23 +21,24 @@ namespace MovieRental.Common.Models
 
         public string Statement()
         {
-            int frequentRenterPoints = 0;
             string result = $"Rental Record for {Name}{Environment.NewLine}";
 
             foreach(var rental in _rentals)
             {
-                // add frequent renter points
-                frequentRenterPoints += rental.FrequentRenterPoints;
-
                 //show figures for this rental
                 result += $"\t{rental.Movie.Title}\t{rental.GetCharge()}{Environment.NewLine}";
             }
 
             //add footer lines
             result += $"Amount owed is {GetTotalCharge()}{Environment.NewLine}";
-            result += $"You earned {frequentRenterPoints} frequent renter points";
+            result += $"You earned {GetTotalFrequentRenterPoints()} frequent renter points";
         
             return result;
+        }
+
+        private int GetTotalFrequentRenterPoints()
+        {
+            return _rentals.Sum(r => r.FrequentRenterPoints);
         }
 
         private double GetTotalCharge()
