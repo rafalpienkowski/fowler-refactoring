ece090e64f3512705103f6cf21e0fc5e91ffa2e3
Rafal Pienkowski <rafal.pienkowski@cognifide.com>
2018-05-15 22:04:51 +0200

    GetCharge moved to Movie from Rental class.

--- src/MovieRental.Common/Models/Movie.cs
+++ src/MovieRental.Common/Models/Movie.cs
@@ -16,5 +16,38 @@ namespace MovieRental.Common.Models
             Title = title;
             PriceCode = priceCode;
         }
+
+        public double GetCharge(int daysRented)
+        {
+            double result = 0;
+            switch (PriceCode)
+            {
+                case Movie.REGULAR:
+                {
+                    result += 2;
+                    if (daysRented > 2)
+                    {
+                        result += (daysRented - 2) * 1.5;
+                    }
+                    break;
+                }
+                case Movie.NEW_RELEASE:
+                {
+                    result += daysRented * 3;
+                    break;
+                }
+                case Movie.CHILDRENS:
+                {
+                    result += 1.5;
+                    if (daysRented > 3)
+                    {
+                        result += (daysRented - 3) * 1.5;
+                    }
+                    break;
+                }
+            }
+
+            return result;
+        }
     }
 }
--- src/MovieRental.Common/Models/Rental.cs
+++ src/MovieRental.Common/Models/Rental.cs
@@ -27,35 +27,7 @@ namespace MovieRental.Common.Models
 
         public double GetCharge()
         {
-            double result = 0;
-            switch (Movie.PriceCode)
-            {
-                case Movie.REGULAR:
-                {
-                    result += 2;
-                    if (DaysRented > 2)
-                    {
-                        result += (DaysRented - 2) * 1.5;
-                    }
-                    break;
-                }
-                case Movie.NEW_RELEASE:
-                {
-                    result += DaysRented * 3;
-                    break;
-                }
-                case Movie.CHILDRENS:
-                {
-                    result += 1.5;
-                    if (DaysRented > 3)
-                    {
-                        result += (DaysRented - 3) * 1.5;
-                    }
-                    break;
-                }
-            }
-
-            return result;
+            return Movie.GetCharge(DaysRented);
         }
     }
 }
