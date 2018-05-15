3e83d33df21ebee59388f751456c2ffb08c1d6f6
Rafal Pienkowski <rafal.pienkowski@cognifide.com>
2018-05-15 22:28:49 +0200

    Apply of getCharge method.

--- src/MovieRental.Common/Models/Movie.cs
+++ src/MovieRental.Common/Models/Movie.cs
@@ -54,35 +54,7 @@ namespace MovieRental.Common.Models
 
         public double GetCharge(int daysRented)
         {
-            double result = 0;
-            switch (PriceCode)
-            {
-                case Movie.REGULAR:
-                {
-                    result += 2;
-                    if (daysRented > 2)
-                    {
-                        result += (daysRented - 2) * 1.5;
-                    }
-                    break;
-                }
-                case Movie.NEW_RELEASE:
-                {
-                    result += daysRented * 3;
-                    break;
-                }
-                case Movie.CHILDRENS:
-                {
-                    result += 1.5;
-                    if (daysRented > 3)
-                    {
-                        result += (daysRented - 3) * 1.5;
-                    }
-                    break;
-                }
-            }
-
-            return result;
+            return price.GetCharge(daysRented);
         }
     }
 }
